658d3c123eb767e3f0d5f8b054f3d55ece124783
Shariq <shariqshaikh2705@gmail.com>
2025-06-21 17:28:47 +0530

    setup MVC structure

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
-import {dotenv} from "dotenv";
+import {config} from "dotenv";
 import express,{json} from "express";
 import { connect } from "mongoose";
 
+config();
 const app = express();
-app.use(express.json());
+app.use(json());
 
 // mongoose.connect(process.env.MONGO_URL,{
 //     useNewUrlParser: true,
@@ -16,7 +17,7 @@ app.get("/",(req,res)=>{
     res.send("Quizzy is UP");
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
-    console.log(`Connected to port ${process.env.PORT}`)
+    console.log(`Connected to port ${PORT}`)
 })
