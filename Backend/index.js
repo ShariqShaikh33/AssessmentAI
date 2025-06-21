7a18a6b1584693e8f00748810daba61acf07dffd
Shariq <shariqshaikh2705@gmail.com>
2025-06-21 21:03:05 +0530

    Setup Complete

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,19 @@
 import {config} from "dotenv";
 import express,{json} from "express";
-import { connect } from "mongoose";
+import mongoose, { connect } from "mongoose";
 
 config();
 const app = express();
 app.use(json());
 
-// mongoose.connect(process.env.MONGO_URL,{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-// .then(()=> console.log("MongoDB Connected"))
-// .catch(()=> console.error("MongoDB Connection error", err));
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("Connected to the Database");
+})
+.catch(()=>{
+    console.error("Cannot connect to the Database");
+});
+
 
 app.get("/",(req,res)=>{
     res.send("Quizzy is UP");
---
+++ Frontend/src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div className='flex justify-center items-center w-full h-full min-h-screen'>
+        <h1 className='font-bold text-3xl'>
+          Welcome to QuizzUP!!
+        </h1>
+    </div>
+  )
+}
+
+export default index
