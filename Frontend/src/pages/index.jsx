aeb6e45141acaf85bb863f817a9c53bcfd4772d6
Shariq <shariqshaikh2705@gmail.com>
2025-08-12 20:32:41 +0530

    added enter button

--- Frontend/src/pages/index.jsx
+++ Frontend/src/pages/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import CustomButton from '../components/common/CustomButton'
 
 function index() {
   return (
-    <div className='flex justify-center items-center w-full h-full min-h-screen'>
+    <div className='flex flex-col justify-center items-center w-full h-full min-h-screen'>
+        
         <h1 className='font-bold text-3xl'>
           Welcome to QuizzUP!!
         </h1>
+        <NavLink to={"/teacher/dashboard"}>
+          <CustomButton>Enter</CustomButton>
+        </NavLink>
     </div>
   )
 }
