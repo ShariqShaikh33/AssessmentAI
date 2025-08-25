45042ef13cc89e67ff222af30150146baeb79be2
Shariq <shariqshaikh2705@gmail.com>
2025-08-25 21:42:52 +0530

    4

--- Frontend/src/pages/Student/submissions/utils/getRowIcon.jsx
+++ Frontend/src/pages/Student/submissions/utils/getRowIcon.jsx
@@ -2,7 +2,7 @@ import { EyeIcon, FilePenLine } from "lucide-react";
 import {
   AssesmentStatusEnum,
   SubmissionStatusEnum,
-} from "../../../../../../backend/types/index";
+} from "../../../../../../Backend/types/index.js";
 
 export const getRowIcon = (status) => {
   switch (status) {
