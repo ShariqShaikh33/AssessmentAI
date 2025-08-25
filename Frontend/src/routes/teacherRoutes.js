ebee6f37d0155f7cc88c3426647f6d05d0bb9e35
Shariq <shariqshaikh2705@gmail.com>
2025-08-25 21:24:46 +0530

    route correction 2

--- Frontend/src/routes/teacherRoutes.js
+++ Frontend/src/routes/teacherRoutes.js
@@ -1,7 +1,7 @@
 export const teacherRoutes = [
   {
     path: "/teacher",
-    element: () => import("../pages/teacher"),
+    element: () => import("../pages/Teacher"),
   },
   // {
   //   path: "/teacher/dashboard",
@@ -10,33 +10,33 @@ export const teacherRoutes = [
   //   Templates Pages
   {
     path: "/teacher/templates",
-    element: () => import("../pages/teacher/templates"),
+    element: () => import("../pages/Teacher/templates"),
   },
   {
     path: "/teacher/templates/create",
-    element: () => import("../pages/teacher/templates/create"),
+    element: () => import("../pages/Teacher/templates/create"),
   },
   {
     path: "/teacher/templates/:id",
-    element: () => import("../pages/teacher/templates/create"),
+    element: () => import("../pages/Teacher/templates/create"),
   },
   //   Assesments Pages
   {
     path: "/teacher/assesments",
-    element: () => import("../pages/teacher/assesments"),
+    element: () => import("../pages/Teacher/assesments"),
   },
   {
     path: "/teacher/assesments/create",
-    element: () => import("../pages/teacher/assesments/create"),
+    element: () => import("../pages/Teacher/assesments/create"),
   },
   {
     path: "/teacher/assesments/:id",
-    element: () => import("../pages/teacher/assesments/create"),
+    element: () => import("../pages/Teacher/assesments/create"),
   },
   // Submissions Pages
   {
     path: "/teacher/submissions",
-    element: () => import("../pages/teacher/submissions"),
+    element: () => import("../pages/Teacher/submissions"),
   },
   {
     path: "/teacher/*",
