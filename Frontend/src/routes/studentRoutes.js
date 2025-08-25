153272f7952b5a112982edde6853581c7e62b877
Shariq <shariqshaikh2705@gmail.com>
2025-08-25 21:23:54 +0530

    route name correction

--- Frontend/src/routes/studentRoutes.js
+++ Frontend/src/routes/studentRoutes.js
@@ -1,7 +1,7 @@
 export const studentRoutes = [
   {
     path: "/student",
-    element: () => import("../pages/student"),
+    element: () => import("../pages/Student"),
   },
   // {
   //   path: "/student/dashboard",
@@ -9,19 +9,19 @@ export const studentRoutes = [
   // },
   {
     path: "/student/assesments",
-    element: () => import("../pages/student/assesments"),
+    element: () => import("../pages/Student/assesments"),
   },
   {
     path: "/student/assesments/:id",
-    element: () => import("../pages/student/assesments/take"),
+    element: () => import("../pages/Student/assesments/take"),
   },
   {
     path: "/student/submissions",
-    element: () => import("../pages/student/submissions"),
+    element: () => import("../pages/Student/submissions"),
   },
   {
     path: "/student/submissions/:id",
-    element: () => import("../pages/student/submissions/submission"),
+    element: () => import("../pages/Student/submissions/submission"),
   },
   {
     path: "/student/*",
